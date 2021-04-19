2f34c4e2f66d97dabe3bd666c55934a078331815
Michael Kalantar <kalantar@us.ibm.com>
2021-04-19 13:59:31 -0400

    remove rbac specific to helm (#57)

--- install-prom-add-on.sh
+++ install-prom-add-on.sh
@@ -2,16 +2,18 @@
 
 set -e
 
-# Step 0: Export TAG
+# Step 0: Export GIT_ACCOUNT and TAG
+# GIT_ACCOUNT enables manual testing from a fork
+export GIT_ACCOUNT="${GIT_ACCOUNT:-iter8-tools}"
 export TAG="${TAG:-v0.3.0}"
 
 # Step 1: Install Prometheus add-on
 # This step assumes you have installed Iter8 using install.sh
 echo "Installing Prometheus add-on"
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/prometheus-operator/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/prometheus-add-on/prometheus-operator/build.yaml
 kubectl wait crd -l creator=iter8 --for condition=established --timeout=120s
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/prometheus/build.yaml
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/prometheus-add-on/service-monitors/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/prometheus-add-on/prometheus/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/prometheus-add-on/service-monitors/build.yaml
 
 echo "Verifying Prometheus-addon installation"
 kubectl wait --for condition=ready --timeout=300s pods --all -n iter8-system
--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-# Step 0: Export TAG
+# Step 0: Export GIT_ACCOUNT and TAG
+# GIT_ACCOUNT enables manual testing from a fork
+export GIT_ACCOUNT="${GIT_ACCOUNT:-iter8-tools}"
 export TAG="${TAG:-v0.3.0}"
 
 # Step 1: Install Iter8
 echo "Installing Iter8"
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/core/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/core/build.yaml
 kubectl wait crd -l creator=iter8 --for condition=established --timeout=120s
-kubectl apply -f https://raw.githubusercontent.com/iter8-tools/iter8-install/${TAG}/metrics/build.yaml
+kubectl apply -f https://raw.githubusercontent.com/${GIT_ACCOUNT}/iter8-install/${TAG}/metrics/build.yaml
 
 echo "Verifying Iter8 installation"
 kubectl wait --for condition=ready --timeout=300s pods --all -n iter8-system
